6bd4ba191d40f1cb03984b85a82908f43ddc8bb3
Emmanuel Dalyop <108156886+EcodeJR@users.noreply.github.com>
2023-02-01 15:31:48 +0100

    Add files via upload

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,8 +2,15 @@ import Nav from "./Nav";
 
 const Home = () => {
     return ( 
-        <div className="absolute top-0 left-0 z-20 bg-[rgba(0, 0, 0, 0.1)] backdrop-blur-xl h-screen w-full text-white">
+        <div className="absolute top-0 left-0 z-20 bg-[rgba(0, 0, 0, 0.1)] backdrop-blur-xl h-full w-full text-white">
             <Nav />
+            <div className="h-[90vh] w-full flex">
+                <div className="text-white w-[50vw] px-10 flex flex-col items-start justify-center">
+                    <h1 className="lg:text-6xl font-bold uppercase">Explore, Keep track of, Manage, buy the latest coins here!</h1>
+                    <p className="text-xl my-3 text-white/70">Be sure to always be well informed of the latest/most expensive/top trending coins out there.</p>
+                    <button className="py-3 px-10 bg-red-500 text-white hover:bg-red-400">EXPLORE</button>
+                </div>
+            </div>
         </div>
      );
 }
